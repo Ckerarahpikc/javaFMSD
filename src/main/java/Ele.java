4cd8d1164cb3e23cf385d6c939b9c782dad4c42a
ckerarahpikc <werrrom@gmail.com>
2026-05-09 13:52:34 +0300

    feat: Ele class to work with
    Ele (Elev) class is just a regular class that I will use for the File
    Manager that we'll be build

---
+++ src/main/java/Ele.java
@@ -0,0 +1,179 @@
+
+// Scrieți un program care permite crearea unei clase simple la tema indicată mai jos, 
+// având ca metode (funcții) citirea și afișarea fiecărui câmp (set-teri și get- teri), precum 
+// și o metodă care calculează o valoare nouă a unui sau a mai multor câmpuri (ca exemplu, 
+// de convertit prețul din lei în EURO sau de calculat durata deplasării). Tema, pe baza 
+// căreia trebuie să creați clasa, precum și câmpurile private obligatorii, sunt indicate în 
+// variantă
+//
+// Elevi (nr, nume, prenume, adresă, telefon, profil, clasă, notă)
+package lab1;
+
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class Ele {
+  private static final int ITEMS_COUNT = 8;
+
+  private String nr;
+  private String nume;
+  private String prenume;
+  private String adresa;
+  private String telefon;
+  private String profil;
+  private int clasa;
+  private double nota;
+
+  public static void main(String[] args) {
+    // System.out.println("The Class is working...");
+    // Ele el = new Ele();
+    //
+    // el.setNr("010.");
+    // el.setFName("Ivanov");
+    // el.setLName("Alexandru");
+    // el.setAddress("Str. Puskin 2/3");
+    // el.setPhone("067 123 4567");
+    // el.setProfile("Real");
+    // el.setYear(11);
+    // el.setGrade(8.45);
+    //
+    // int fullNameLength = el.getNameLength();
+    // System.out.println("Name length: " + fullNameLength);
+    // el.getPerson();
+    //
+    // or use getters
+
+    // System.out
+    // .println("\n" + el.getNr() + "\n" + el.getFName() + "\n" + el.getLName() +
+    // "\n" + el.getAddress() + "\n"
+    // + el.getPhone() + "\n" + el.getProfile() + "\n"
+    // + el.getYear() + "\n" + el.getGrade());
+  }
+
+  public Ele(String nr, String nume, String prenume, String adresa, String telefon, String profil, int clasa,
+      double nota) {
+    setNr(nr);
+    setFName(nume);
+    setLName(prenume);
+    setAddress(adresa);
+    setPhone(telefon);
+    setProfile(profil);
+    setYear(clasa);
+    setGrade(nota);
+  }
+
+  public Ele() {
+    this("000.", "Nespecificat", "Nespecificat", "Nespecificat", "0000 00 000", "Uman", 0, 0.00);
+  }
+
+  // setter
+  public void setNr(String nr) {
+    if (nr == null || nr.trim().isEmpty()) {
+      throw new Error("Invalid Expression.");
+    }
+    this.nr = nr;
+  }
+
+  public void setFName(String nume) {
+    if (nume == null || nume.trim().isEmpty()) {
+      throw new Error("Invalid Expression.");
+    }
+    this.nume = nume;
+  }
+
+  public void setLName(String prenume) {
+    if (prenume == null || prenume.trim().isEmpty()) {
+      throw new Error("Invalid Expression.");
+    }
+    this.prenume = prenume;
+  }
+
+  public void setAddress(String adresa) {
+    if (adresa == null || adresa.trim().isEmpty()) {
+      throw new Error("Invalid Expression.");
+    }
+    this.adresa = adresa;
+  }
+
+  public void setPhone(String telefon) {
+    if (telefon == null || telefon.trim().isEmpty()) {
+      throw new Error("Invalid Expression.");
+    }
+    this.telefon = telefon;
+  }
+
+  public void setProfile(String profil) {
+    if (profil == null || profil.trim().isEmpty()) {
+      throw new Error("Invalid Expression.");
+
+    }
+    this.profil = profil;
+  }
+
+  public void setYear(int clasa) {
+    if (clasa <= 0 || clasa >= 13) {
+      throw new Error("Invalid Expression.");
+
+    }
+    this.clasa = clasa;
+  }
+
+  public void setGrade(double nota) {
+    if (nota <= 0 || nota >= 11) {
+      throw new Error("Invalid Expression.");
+    }
+    this.nota = nota;
+  }
+
+  // getters
+  final public Integer getItemsCount() {
+    return ITEMS_COUNT;
+  }
+
+  public String getNr() {
+    return nr;
+  }
+
+  public String getFName() {
+    return nume;
+  }
+
+  public String getLName() {
+    return prenume;
+  }
+
+  public String getAddress() {
+    return adresa;
+  }
+
+  public String getPhone() {
+    return telefon;
+  }
+
+  public String getProfile() {
+    return profil;
+  }
+
+  public Integer getYear() {
+    return clasa;
+  }
+
+  public Double getGrade() {
+    return nota;
+  }
+
+  // method
+  public Integer getNameLength() {
+    return (nume + prenume).trim().length();
+  }
+
+  public void getPerson() {
+    System.out.println("\n" + nr + "\n" + nume + "\n" + prenume + "\n" + adresa + "\n" + telefon + "\n" + profil + "\n"
+        + clasa + "\n" + nota);
+  }
+
+  @Override
+  public String toString() {
+    return nume + " : " + nota;
+  }
+}
